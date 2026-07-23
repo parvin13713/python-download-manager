e916f0d9b685f66079240335cceb58e84640a296
Parvin <parvin13713@gmail.com>
2026-07-23 23:17:12 +0430

    Add download progress bar

--- downloader.py
+++ downloader.py
@@ -1,14 +1,30 @@
 import requests
+from urllib.parse import urlparse
 
 
-def download_file(url):
-    filename = url.split("/")[-1]
+def download_file(url, progress_callback):
+    parsed_url = urlparse(url)
+
+    filename = parsed_url.path.split("/")[-1]
+
+    if not filename:
+        filename = "downloaded_file"
 
     response = requests.get(url, stream=True)
+
+    total_size = int(response.headers.get("content-length", 0))
+
+    downloaded = 0
 
     with open(filename, "wb") as file:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk:
                 file.write(chunk)
+
+                downloaded += len(chunk)
+
+                if total_size:
+                    percent = downloaded * 100 / total_size
+                    progress_callback(percent)
 
     return filename
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from downloader import download_file
 import tkinter as tk
+from tkinter import ttk
+from downloader import download_file
+
+
+def update_progress(value):
+    progress_bar["value"] = value
+    window.update_idletasks()
+
 
 def start_download():
     url = url_entry.get()
 
-    filename = download_file(url)
+    filename = download_file(url, update_progress)
 
     status_label.config(
         text=f"Downloaded: {filename}"
@@ -13,7 +20,7 @@ def start_download():
 
 window = tk.Tk()
 window.title("Mini Download Manager")
-window.geometry("500x250")
+window.geometry("500x300")
 
 
 label = tk.Label(window, text="Download URL:")
@@ -31,6 +38,16 @@ download_button = tk.Button(
 )
 download_button.pack(pady=20)
 
+
+progress_bar = ttk.Progressbar(
+    window,
+    length=400
+)
+progress_bar.pack(pady=10)
+
+
 status_label = tk.Label(window, text="")
 status_label.pack()
+
+
 window.mainloop()
